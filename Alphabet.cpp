689aba4342b81f6f7e0672c95dd9a164de0119b1
Mudasir Ahmad <mudasirofficially@gmail.com>
2022-12-16 04:53:54 -0800

    PF Week02 Lab PD Day

---
+++ Alphabet.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+using namespace std;
+main(){
+cout << "             #              " << endl;
+cout << "            # #            " << endl;
+cout << "           #   #           " << endl;
+cout << "          #     #          " << endl;
+cout << "         #       #         " << endl;
+cout << "        #         #        " << endl;
+cout << "       #############       " << endl;
+cout << "      #             #      " << endl;
+cout << "     #               #     " << endl;
+cout << "    #                 #    " << endl;
+cout << "   #                   #   " << endl;
+cout << "  #                     #  " << endl;
+cout << " #                       # " << endl;
+cout << endl;
+cout << endl;
+cout << endl;
+cout << endl;
+
+
+cout << "    #          #                   " << endl;
+cout << "     #        #                     " << endl;
+cout << "      #      #                    " << endl;
+cout << "       #    #                     " << endl;
+cout << "        #  #                         " << endl;
+cout << "          #                     " << endl;
+cout << "         # #             " << endl;
+cout << "        #   #               " << endl;
+cout << "       #     #                  " << endl;
+cout << "      #       #     " << endl;
+cout << "     #         #       " << endl;
+cout << "    #           #    "<<endl;
+cout << endl;
+cout << endl;
+cout << endl;
+cout << endl;
+
+cout  << "# # # # # # # # # # # # #" << endl;
+cout<<"                           #"<<endl;
+cout<<"                          #"<<endl;
+cout<<"                         #"<<endl; 
+cout<<"                        #"<<endl;
+cout<<"                       #"<<endl;
+cout<<"                      #"<<endl;
+cout<<"                     #"<<endl;
+cout<<"                    #"<<endl;
+cout<<"                   #"<<endl;
+cout<<"                  #"<<endl;
+cout<<"                 #"<<endl;
+cout<<"                #"<<endl;
+cout<<"               #"<<endl;
+cout<<"              #"<<endl;
+cout<<"             #"<<endl;
+cout<<"            #"<<endl;
+cout<<"           #"<<endl;
+cout<<"          #"<<endl;
+cout<<"         #"<<endl;
+cout<<"        #"<<endl;
+cout<<"       #"<<endl;
+cout<<"      #"<<endl;
+cout<<"     #"<<endl;
+cout  << "# # # # # # # # # # # # #" << endl;
+cout << endl;
+cout << endl;
+cout << endl;
+cout << endl;
+
+cout << "k      K "<< endl;
+cout << "k     k "<< endl;
+cout << "k    k  "<< endl;
+cout << "k   k   "<< endl;
+cout << "k  k    " << endl;
+cout << "k k     "  << endl;
+cout << "kk      "<< endl;
+cout << "k  k      "<< endl;
+cout << "k   k      "<< endl;
+cout << "k    k     "<< endl;
+cout << "k     k     "<< endl;
+cout << endl;
+cout << endl;
+cout << endl;
+cout << endl;
+cout << "      sssssss       "<<endl;
+cout << "     s        s     "<<endl;
+cout << "    s          s   "<<endl;
+cout << "     s             "<<endl;
+cout << "      s           "<<endl;
+cout << "       ssssss       "<<endl;
+cout << "             s    "<<endl;
+cout << "              s     "<<endl;
+cout << "              s       "<<endl;
+cout << "             s     "<<endl;
+cout << "     ssssssss      "<<endl;
+cout << endl;
+cout << endl;
+cout << endl;
+cout << endl;
+cout << "u               u     "<< endl;
+cout << "u               u     "<< endl;
+cout << "u               u     "<< endl;
+cout << "u               u     "<< endl;
+cout << "u               u    "<< endl;
+cout << "u               u    "<< endl;
+cout << "u               u    "<< endl;
+cout << "u               u      "<< endl;
+cout << "u               u         "<< endl;
+cout << " u             u            "<< endl;
+cout << "   u u u u u u                "<< endl;
+}
+
+
+
+
+
+
---
+++ UAMS_color.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+main(){
+system("Color 20");
+cout << "u         u         A       M       M      sssssss                  "<< endl;
+cout << "u         u        A A      M M   M M     s       s                       "<< endl;
+cout << "u         u       A   A     M   M   M     s                                     "<< endl;
+cout << "u         u      AAAAAAA    M       M     sssssss                             "<< endl;
+cout << "u         u     A       A   M       M            s                       "<< endl;
+cout << " u       u     A         A  M       M            s                       "<< endl;
+cout << "  uuuuuuu     A           A M       M     sssssss                            "<< endl;
+
+
+
+
+}
